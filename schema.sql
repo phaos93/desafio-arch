489ea19a422dea769eb93d81d8655412574ac448
Pedro Silva <phaos93@gmail.com>
2022-11-07 11:17:20 -0300

    feat: schema criado para criação das tabelas no banco de dados postgresSQL

---
+++ schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE usuarios
+(
+    id SERIAL PRIMARY KEY,
+    nome TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    senha VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE transacoes
+(
+    id SERIAL PRIMARY KEY,
+    valor INT NOT NULL,
+    descricao TEXT NOT NULL,
+  	data_transacao DATE NOT NULL DEFAULT NOW(),
+  	tipo VARCHAR(8),
+  	usuario_id INT NOT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);
