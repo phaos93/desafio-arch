fae72098dcae2433f66ec70c9db2064ba2a4d57b
Pedro Silva <phaos93@gmail.com>
2022-11-07 10:57:54 -0300

    chore: API de transações criada e pacotes instalados

---
+++ transacao-api/src/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const express = require("express");
+const cors = require("cors")
+
+const app = express();
+app.use(cors())
+
+app.use(express.json());
+
+app.listen(process.env.PORT || 3000);
