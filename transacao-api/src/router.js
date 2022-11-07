990a9c6557c2841ab790cf4b669aea79e7c6ae59
Pedro Silva <phaos93@gmail.com>
2022-11-07 17:13:52 -0300

    feat: criando o middleware para fazer a vefiricação do token

--- transacao-api/src/router.js
+++ transacao-api/src/router.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const users = require("./controllers/users");
+const verifyLogin = require("./filters/verifyLogin");
 
 const router = express();
 
 router.post('/users', users.registerUser);
 router.post('/login', users.loginUser);
+
+router.use(verifyLogin);
 
 module.exports = router;
