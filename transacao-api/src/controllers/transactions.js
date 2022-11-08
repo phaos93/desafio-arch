9d99a14c898f417fcd337a22ed3edebe6c8006e9
Pedro Silva <phaos93@gmail.com>
2022-11-08 15:00:48 -0300

    feat: adicionando padrão de retry no registro de transações

--- transacao-api/src/controllers/transactions.js
+++ transacao-api/src/controllers/transactions.js
@@ -4,29 +4,36 @@ const { schemaRegisterTransactions } = require('../validations/schemaTransaction
 const registerTransaction = async (req, res) => {
     const { user } = req;
     const { valor, descricao, tipo } = req.body;
+    let currentTry = 0;
 
-    try {
-        await schemaRegisterTransactions.validate(req.body);
-        if (tipo !== 'entrada' && tipo !== 'saida') {
-            return res.status(400).json("o tipo da transação deve ser 'entrada' ou 'saida'");
+    while (currentTry < 6 ) {
+        try {
+            await schemaRegisterTransactions.validate(req.body);
+            if (tipo !== 'entrada' && tipo !== 'saida') {
+                return res.status(400).json("o tipo da transação deve ser 'entrada' ou 'saida'");
+            }
+
+            const transaction = await knex('transacoes')
+                .insert({
+                    "valor": valor.toFixed(2),
+                    descricao,
+                    tipo,
+                    "usuario_id": user[0].id
+                });
+
+            if (transaction.length === 0) {
+                return res.status(400).json("Não foi possivel cadastrar a transação");
+            }
+
+            return res.status(201).json("Transação efetuada com sucesso")
+
+        } catch (e) {
+            currentTry++;
+            console.log(currentTry);
+            if (currentTry === 5) {
+                return res.status(500).json(e.message);
+            }
         }
-
-        const transaction = await knex('transacoes')
-            .insert({
-                "valor": valor.toFixed(2),
-                descricao,
-                tipo,
-                "usuario_id": user[0].id
-            });
-
-        if (transaction.length === 0) {
-            return res.status(400).json("Não foi possivel cadastrar a transação");
-        }
-
-        return res.status(201).json("Transação efetuada com sucesso")
-
-    } catch (e) {
-        return res.status(500).json(e.message);
     }
 };
 
