64e52645e7efd78d92b74affe932f77ecb15414a
Pedro Silva <phaos93@gmail.com>
2022-11-07 22:03:40 -0300

    feat: configurando arquivo de rotas no index.js

--- saldo-api/src/index.js
+++ saldo-api/src/index.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const express = require("express");
 const cors = require("cors");
+const router = require("./router");
 
 const app = express();
 app.use(cors())
 
 app.use(express.json());
+app.use(router);
 
 app.listen(3000);
